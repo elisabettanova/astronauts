e945e8a9bfbfb8292035953eadffccf455f54043
Midori Kocak <mtkocak@gmail.com>
2021-09-25 14:35:07 +0300

    One server

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
 const axios = require("axios");
 const http = require("http");
+const fs = require('fs')
 
 let handleRequest = (httpRequest, httpResponse) => {
   const url = "http://api.open-notify.org/astros.json";
 
-  axios
-  .get(url)
-  .then((axiosResponse) => {
-    let data = axiosResponse.data;
-    let people = data.people;
+  console.log(httpRequest.url);
 
-    httpResponse.writeHead(200, {
-        'Content-Type': 'application/json',
-    });
-
-    httpResponse.write(JSON.stringify(people));
-    httpResponse.end();
-  })
-  .catch((err) => {
-    httpResponse.write("an error happened" + err.message);
-    httpResponse.end();
-  });
-
-}
-
-  /*
-  axios
+  if(httpRequest.url === '/' || httpRequest.url === 'index.html'){
+    httpResponse.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('index.html').pipe(httpResponse)
+  }
+  else if(httpRequest.url === '/data.json'){
+    axios
     .get(url)
     .then((axiosResponse) => {
       let data = axiosResponse.data;
+      let people = data.people;
+  
+      httpResponse.writeHead(200, {
+          'Content-Type': 'application/json',
+          // We use this when there is no data manipulation, (NO POST, PUT, PATCH, DELETE)
+          // 'Access-Control-Allow-Origin': '*' 
+      });
+  
+      httpResponse.write(JSON.stringify(people));
+      httpResponse.end();
     })
     .catch((err) => {
+      httpResponse.write("an error happened" + err.message);
+      httpResponse.end();
     });
-};
-*/
+  }
+}
 
 http.createServer(handleRequest).listen(8000);
